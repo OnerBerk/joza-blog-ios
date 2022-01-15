531289923359c5f1ba05677db5123fa5d2ca72cc
OnerBerk <oner.berk@joza-it.fr>
2022-01-15 14:51:00 +0100

    Initial Commit

---
+++ ClientMobile/ClientMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClientMobileApp.swift
+//  ClientMobile
+//
+//  Created by onAir on 15/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct ClientMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
